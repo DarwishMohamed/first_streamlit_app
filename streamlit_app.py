01854b583f2f35b2ac4243dd2fad7851f9c1a3f2
DarwishMohamed <109226826+DarwishMohamed@users.noreply.github.com>
2023-10-03 15:58:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,13 @@
 import streamlit 
-streamlit = st
+
 
 streamlit.title("My Parents New Healthy Diner")
 
 streamlit.subheader('Breakfast menu')
 
-st.write('Omega 3 & Blueberry Oatemal') 
+streamlit.write('Omega 3 & Blueberry Oatemal') 
 
-st.write('Kale, Spinach & Rocket Smoothie') 
+streamlit.write('Kale, Spinach & Rocket Smoothie') 
 
-st.write('Hard-Boiled Free-Range Egg')
+streamlit.write('Hard-Boiled Free-Range Egg')
 
