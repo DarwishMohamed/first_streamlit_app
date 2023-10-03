a9b054d19384be8a3199c5629ae66f18faf39689
DarwishMohamed <109226826+DarwishMohamed@users.noreply.github.com>
2023-10-03 15:57:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,11 @@ import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
 
-streamlit.subtitle("Breakfast Menu") 
+streamlit.subheader('Breakfast menu')
+
+st.write('Omega 3 & Blueberry Oatemal') 
+
+st.write('Kale, Spinach & Rocket Smoothie') 
+
+st.write('Hard-Boiled Free-Range Egg')
+
