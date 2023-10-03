469073786b926db76b54be76d7c89765fd6964e2
DarwishMohamed <109226826+DarwishMohamed@users.noreply.github.com>
2023-10-03 16:12:01 +0100

    Update streamlit_app.py
     make the table below the picker a bit smarter so it's doesn't load all the fruits, just the ones shown in the pick list.

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,3 +23,8 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 #Display the table on the page
 streamlit.dataframe(my_fruit_list)
+
+
+# Lets put a pick list here so tehy can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
+
