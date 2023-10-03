dd51a08053e2cff363b6756ac8a3147577a6d88e
DarwishMohamed <109226826+DarwishMohamed@users.noreply.github.com>
2023-10-03 16:07:42 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit as st
+import streamlit 
 
 
 streamlit.title("My Parents New Healthy Diner")
@@ -11,7 +11,7 @@ streamlit.write('Kale, Spinach & Rocket Smoothie')
 
 streamlit.write('Hard-Boiled Free-Range Egg')
 
-import pandas as pd
+import pandas 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit') 
 streamlit.dataframe(my_fruit_list) 
