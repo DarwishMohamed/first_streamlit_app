a882b708a1403863a4b49a05288f78c341cb6b28
DarwishMohamed <109226826+DarwishMohamed@users.noreply.github.com>
2023-10-03 16:07:15 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,16 +12,13 @@ streamlit.write('Kale, Spinach & Rocket Smoothie')
 streamlit.write('Hard-Boiled Free-Range Egg')
 
 import pandas as pd
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit') 
+streamlit.dataframe(my_fruit_list) 
 
 
-my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
+# lets puck a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
-# Display the table on the page
-st.dataframe(my_fruit_list)
-
-# Let's put a pick list here so they can pick the fruit they want to include 
-selected_fruits = st.multiselect("Pick some fruits:", list(my_fruit_list.index))
-
-# If you want to do something with the selected fruits, you can use the `selected_fruits` variable.
-
+#Display the table on the page
+streamlit.dataframe(my_fruit_list)
